ad4b59a32c3d745da7786ec0665a659d68e28c5a
Vladimir <vladimirleontiev.devmail@gmail.com>
2021-03-03 23:43:45 +0300

    feat: add start animation

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import slyles from './Footer.module.css';
+import animate from '../animate.module.css';
 
 const Footer = () => {
+  const animation = `${animate.animate__animated} ${animate.animate__fadeIn}`;
+  const cssClasses = `${slyles.Footer} ${animation}`;
   return (
-    <div className={slyles.Footer}>
-      <a className={`${slyles.rsLogo} unselectable`} href={'https://rs.school/js'} rel="noreferrer" target={'_blank'}>
-        <object type="image/svg+xml" data="./img/rs_school_js-logo.svg" className={`${slyles.rsSVG} unselectable`} >
-          Your browser does not support SVG
-        </object>
-      </a>
-      <div className={`${slyles.description} unselectable`}>
-        <span>
-          Produced by Vladimir Leontiev as part of the training course "The Rolling Scopes"
-        </span>
-        <span>2021</span>
-      </div>
+    <div className={cssClasses}>
+      <div className={`${animate.animate__animated} ${animate.animate__fadeIn}`}>
+        <a className={`${slyles.rsLogo} unselectable`} href={'https://rs.school/js'} rel="noreferrer" target={'_blank'}>
+          <object type="image/svg+xml" data="./img/rs_school_js-logo.svg" className={`${slyles.rsSVG} unselectable`} >
+            Your browser does not support SVG
+          </object>
+        </a>
 
-      <a  className={`${slyles.githubLogo} unselectable`} 
-          href={'https://github.com/vladimirleontev281/react-game'} 
-          rel="noreferrer" 
-          target={'_blank'}
-      >
-        <img src={'./img/GitHub_Logo_White.png'} className={slyles.githubImg} alt='GitHub logo' />
-      </a>
+        <div className={`${slyles.description} unselectable`}>
+          <span>
+            Produced by Vladimir Leontiev as part of the training course "The Rolling Scopes"
+          </span>
+          <span>2021</span>
+        </div>
+
+        <a  className={`${slyles.githubLogo} unselectable`} 
+            href={'https://github.com/vladimirleontev281/react-game'} 
+            rel="noreferrer" 
+            target={'_blank'}
+        >
+          <img src={'./img/GitHub_Logo_White.png'} className={slyles.githubImg} alt='GitHub logo' />
+        </a>
+      </div>
     </div>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
 // import { actionCreators } from "../../store/reduser";
 import slyles from './Header.module.css';
+import animate from '../animate.module.css';
 
 const Header = ({thunks}) => {
-
+  const animation = `${animate.animate__animated} ${animate.animate__fadeIn}`;
+  // const animation = `${animate.animate__animated} ${animate.animate__slideInRight}`;
+  const cssClasses = `${slyles.Header} ${animation}`;
   return (
-    <div className={slyles.Header}>
+    <div className={cssClasses}>
       <div className={slyles.logoBlock}>
-        <span className='unselectable'>Tic</span>
-        <span className='unselectable'>Tac</span>
-        <span className='unselectable'>Toe</span>
+        <span className={`unselectable ${animate.animate__animated} ${animate.animate__zoomIn}`}>Tic</span>
+        <span className={`unselectable ${animate.animate__animated} ${animate.animate__zoomIn}`}>Tac</span>
+        <span className={`unselectable ${animate.animate__animated} ${animate.animate__zoomIn}`}>Toe</span>
       </div>
-      <ul className={slyles.menuBlock} >
+      <ul className={`${slyles.menuBlock} ${animate.animate__animated} ${animate.animate__fadeIn}`} >
         <li className='unselectable' onClick={() => {thunks.resetScore()}}>Reset the score</li>
         <li className='unselectable' onClick={() => {thunks.clearBase()}}>Clear all statistics</li>
       </ul>
